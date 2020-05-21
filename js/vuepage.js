df5319a732f9675d87783358de7ffd5bf5a1ae12
wjmwjm119 <wjmmail@163.com>
2020-05-21 19:11:13 +0800

    Revert "wjm20200521"
    This reverts commit d5cef2663fd754f13d449cd8b712e9f3dcdcdd2a.

--- js/vuepage.js
+++ js/vuepage.js
@@ -79,10 +79,10 @@ let logopage = new Vue({
             loadingpage.FadeIn({
                 onFadeInEnd: () =>
                 {
-                     XR.LoadSceneLoop(["main", "美术关卡", "Night", "mp_110", "mp_140", "mp_125", "jgmy_xlz"],
-                         "", "", XR.CallBack("JsRun", 'XR.SetActiveSceneInstance("main");loadingpage.FadeOut();mainpage.FadeIn();mediapage.FadeIn();'));
-                    //XR.LoadSceneLoop(["main", "A1", "A2", "b", "C_D", "E", "KP_XP", "NDX", "nw_shu_dd", "PG_GQ", "ww_dx", "ww_dx_JRBK", "ww_dx_WWBK", "ww_jz", "美术关卡", "Night", "mp_110", "mp_140", "mp_125", "jgmy_xlz"],
-                    //    "", "", XR.CallBack("JsRun", 'XR.SetActiveSceneInstance("main");loadingpage.FadeOut();mainpage.FadeIn();mediapage.FadeIn();'));
+                    // XR.LoadSceneLoop(["main", "美术关卡", "Night", "mp_110", "mp_140", "mp_125", "jgmy_xlz"],
+                    //     "", "", XR.CallBack("JsRun", 'XR.SetActiveSceneInstance("main");loadingpage.FadeOut();mainpage.FadeIn();mediapage.FadeIn();'));
+                    XR.LoadSceneLoop(["main", "A1", "A2", "b", "C_D", "E", "KP_XP", "NDX", "nw_shu_dd", "PG_GQ", "ww_dx", "ww_dx_JRBK", "ww_dx_WWBK", "ww_jz", "美术关卡", "Night", "mp_110", "mp_140", "mp_125", "jgmy_xlz"],
+                        "", "", XR.CallBack("JsRun", 'XR.SetActiveSceneInstance("main");loadingpage.FadeOut();mainpage.FadeIn();mediapage.FadeIn();'));
                 }
             });
 
@@ -681,10 +681,10 @@ let xfpage = new Vue({
         requirTitleOrder: ["0", "4", "5", "6", "7", "8"],
         //对应数据文字list
         titlelabel: ["号楼", "单元", "楼层", "房间", "户型", "规格", "类型", "朝向", "销售状态"],
-        btnminwidth: ["95px", "100px", "100px", "150px", "120px", "250px", "250px", "120px", "100px"],
+        btnminwidth: ["85px", "100px", "100px", "170px", "120px", "190px", "120px", "120px", "100px"],
         addlabel: [" #", "", "", "", "", "", "", "", ""],
         //每一项显示多少按钮
-        displaycount: [10, 10, 10, 10, 10, 4, 10, 10, 10],
+        displaycount: [3, 10, 10, 10, 10, 10, 10, 10, 10],
 
         //楼层映射表,替换传进来的floor
         floorRemap: ["2-5", "6-10", "11-15", "16-20", "21-25", "26-30", "31-35", "36-40"],
@@ -716,6 +716,8 @@ let xfpage = new Vue({
         lognumber: 0,
 
 
+
+
         ////////////CH
         hxcgSrc: "", //CH
         hxcgbg: "",
@@ -735,12 +737,11 @@ let xfpage = new Vue({
         hxxzinfoSrc: "",
         isEnterroom: false,
 
+
+
         nextbtnstat: false,
         perbtnstat: false,
 
-        xfmenurectStyle:"",
-        xfmenurectStyle2:"",       
-        xfmenurectStyle3:"",
 
         ///////////////////////////////////////CH
         selectbuilding: (btn) =>
@@ -794,25 +795,15 @@ let xfpage = new Vue({
             switch (this.enterType)
             {
                 case 0:
-
-                    this.xfmenurectStyle="width: 100%;height:100%; display: flex;flex-direction: column;"
-                    this.xfmenurectStyle2="margin-top: 0px";
-
                     this.$refs.hxxzbtngrouprect.PlayAni(true, "", "right:0%");
                     XR.SetSceneActorState("loubiao", true);
                     XR.SetCameraPositionAndxyzCount("34532.46875,-25131.072266,126.574028,-113.239761,46.75,39999.992188");
                     break;
                 case 1:
-                    this.xfmenurectStyle="width: 100%; display: flex;flex-direction: column;"
-                    this.xfmenurectStyle2="margin-top: 10px";
-
                     this.$refs.xfmenurect.PlayAni(true, "", "right:0%");
                     XR.SetCameraPositionAndxyzCount("34532.46875,-25131.072266,126.574028,-113.239761,46.75,39999.992188");
                     break;
                 case 2:
-                    this.xfmenurectStyle="width: 100%; display: flex;flex-direction: column;"
-                    this.xfmenurectStyle2="margin-top: 10px";
-
                     this.$refs.xfmenurect.PlayAni(true, "", "right:0%");
                     XR.SetCameraPositionAndxyzCount("34532.46875,-25131.072266,126.574028,-113.239761,46.75,39999.992188");
                     break;
@@ -825,7 +816,6 @@ let xfpage = new Vue({
 
             if (this.$refs.hxxzbtnrootgroup.cposition < this.$refs.hxxzbtnrootgroup.btncount)
                 this.nextbtnstat = true;
-
         },
         TimeLoopHttp(time)
         {
